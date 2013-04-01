46436a682fd55634c315521bfe2edd353d89a5ce
Abhinav Prakash <abhinav.prakash@inmobi.com>
2013-04-01 11:16:13 +0530

    added guice examples

---
+++ examples/src/main/java/com/abhinavp/guice/App.java
@@ -0,0 +1,13 @@
+package com.abhinavp.guice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
